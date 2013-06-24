238bc0392cc2f7711c20cbfa1aa2411425fcfecd
Alexander Shirokov <keyzi.ru@gmail.com>
2013-06-24 23:42:21 +0400

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
-var express = require('express');
+//var express = require('express');
+//
+var fs = require('fs');
+var outfile = "hello.txt";
+var out = "A startup is a business built to grow rapidly.\n";
+fs.writeFileSync(outfile, out);
+console.log("Script: " + outfile + "\nWrote: " + out + "To: " + outfile);
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
